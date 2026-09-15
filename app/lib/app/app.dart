853c5d513503afe9b9c4ed069a8ba182eb021295
Fawad Shah <fawadshah2117@gmail.com>
2026-09-15 13:17:45 +0400

    chore: added a print

--- app/lib/app/app.dart
+++ app/lib/app/app.dart
@@ -5,6 +5,8 @@ class CadenceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List items = [];
+    print(items.length);
     return const MaterialApp(
       title: 'Cadence',
       home: Scaffold(body: Center(child: Text('Cadence'))),
